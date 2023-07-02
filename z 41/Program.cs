e06dd2bceba8a37ffb7b9b02a08f0cdc5b1473f9
Irina_Leskova <krip@mail.ru>
2023-07-02 15:26:40 +0500

    Ничего не получается

---
+++ z 41/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+Console.Clear();
+int [] arrey = GetArray (5,0,10);
+Console.Write(String.Join(" ", array));
+Console.Write(String.Join(" ", ReservArray2(array)));
+Console.Write(String.Join(" ", ReservArray1(array)));
+Console.Write(String.Join(" ", array));
+int[] GetArray (int size, int minValue, int maxValue); 
+int[]res = new Array[size];
+for (int a = 0; i<size ; i++)
+{
+int[] res = new Array[size];
+}
+
+return res;
+
+{ 
+    for (int i = 0; i < size; i++)
+    {
+           if (a<0) 
+           Console.Write($"Количество положительных чисел{i+1}");
+    }   
+}
